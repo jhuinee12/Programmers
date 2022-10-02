7de44b13ddb9e6059f33307bad61f4407c190b4d
jhuinee12 <jhuinee12@gmail.com>
2022-10-02 15:41:11 +0900

    [DEV-6] Kotlin - 코딩 테스트 공부 (Lv3) 문제 주석 추가

--- programmers-source/programmers-level3/src/main/kotlin/StudyingForCodingTest.kt
+++ programmers-source/programmers-level3/src/main/kotlin/StudyingForCodingTest.kt
@@ -1,7 +1,5 @@
 package src.main.kotlin
 
-import kotlin.math.max
-
 /**
  * Title    : 코딩 테스트 공부
  * Url      : https://school.programmers.co.kr/learn/courses/30/lessons/118668?language=kotlin
@@ -23,8 +21,7 @@ class StudyingForCodingTest {
     // 1. 알고력과 코딩력이 problems에서 가장 높은 alp_req, cop_req으로 달성해야 함
     // 2. [alp,cop]에서 [alp_req,cop_req]로 가는 최단 시간을 구해야 함
     // 3. dp[i][j] : [목표알고력, 목표코딩력]이 되는 최단 시간
-    
-    // 4,7,8,10번 런타임 에러 -> 초기값 세팅 에러
+
     fun solution(alp: Int, cop: Int, problems: Array<IntArray>): Int {
         val maxAlp = problems.maxOf { it[0] }
         val maxCop = problems.maxOf { it[1] }
@@ -34,6 +31,7 @@ class StudyingForCodingTest {
         }
 
         // DP 배열의 값은 제일 큰 값으로 초기화하고 초기 알고력/코딩력은 0으로 초기화
+        // 4,7,8,10번 런타임 에러 -> 초기값 세팅 에러
         val dp = Array(maxAlp + 1){ Array(maxCop + 1){ Integer.MAX_VALUE } }
         val initA = if (alp > maxAlp) maxAlp else alp
         val initC = if (cop > maxCop) maxCop else cop
@@ -68,5 +66,5 @@ class StudyingForCodingTest {
 fun main() {
     println(StudyingForCodingTest().solution(10, 10, arrayOf(intArrayOf(10,15,2,1,2), intArrayOf(20,20,3,3,4)))) // 정답: 15
     println(StudyingForCodingTest().solution(0, 0, arrayOf(intArrayOf(0,0,2,1,2), intArrayOf(4,5,3,1,2), intArrayOf(4,11,4,0,2), intArrayOf(10,4,0,4,2)))) // 정답: 13
-    println(StudyingForCodingTest().solution(12, 0, arrayOf(intArrayOf(0,0,2,1,2), intArrayOf(4,5,3,1,2), intArrayOf(4,11,4,0,2), intArrayOf(10,4,0,4,2)))) // 정답: 13
+    println(StudyingForCodingTest().solution(12, 0, arrayOf(intArrayOf(0,0,2,1,2), intArrayOf(4,5,3,1,2), intArrayOf(4,11,4,0,2), intArrayOf(10,4,0,4,2))))
 }

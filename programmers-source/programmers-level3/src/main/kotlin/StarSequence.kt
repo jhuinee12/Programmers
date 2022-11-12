3613fe95b9ee26aaf860434168430f07373963b8
jhuinee12 <jhuinee12@gmail.com>
2022-11-12 20:34:05 +0900

    [ALG-P-22] 스타 수열 (Lv3) 문제 풀이

--- programmers-source/programmers-level3/src/main/kotlin/StarSequence.kt
+++ programmers-source/programmers-level3/src/main/kotlin/StarSequence.kt
@@ -1,30 +1,48 @@
 package src.main.kotlin
 
 class StarSequence {
+    /**
+     * 원래 시간초과(14-15번) 났던 이유 :: index의 이동과정을 알기 위해 contains를 사용했음.
+     * 굳이 contains를 사용할 필요 없이 가장 마지막 배열의 요소를 확인하면 됨.
+     *
+     * 풀이
+     * 1. a의 사이즈만큼의 이중 배열 array를 생성함 (index의 이동을 저장하기 위함)
+     * 2. a의 요소를 확인하는 반복문 생성
+     * 3. 조건문
+     *  - 현재 index가 0(제일 앞)인 경우
+     *    - array[현재요소]에 현재 index와 다음 index 추가
+     *  - 현재 index가 last인 경우
+     *    - (이전 index가 추가되어 있지 않은 경우!!) array[현재요소]에 현재 index와 이전 index 추가
+     *  - 그 외
+     *    - (다음 index가 추가되어 있지 않은 경우!!) array[현재요소]에 현재 index와 다음 index 추가
+     *    - (이전 index가 추가되어 있지 않은 경우!!) array[현재요소]에 현재 index와 이전 index 추가
+     * 4. array[현재요소]의 크기가 짝수이고 answer보다 큰 경우 asnwer를 바꿔줌
+     */
     fun solution(a: IntArray): Int {
         if (a.size < 2) return 0
 
         var answer = 0
 
-        val array = Array(500000) { ArrayList<Int>() }
+        val array = Array(a.size) { ArrayList<Int>() }
 
         a.forEachIndexed { idx, i ->
             if (answer > array[i].size+a.size-idx) return@forEachIndexed // 시간초과 방지
 
+            val lastIndex = if (array[i].size != 0) array[i][array[i].size-1] else -1
             when (idx) {
                 0 -> if (a[idx+1] != i) {
                     array[i].add(idx)
                     array[i].add(idx+1)
                 }
-                a.size-1 -> if (a[idx-1] != i && !array[i].contains(idx-1)) {
+                a.size-1 -> if (a[idx-1] != i && lastIndex != idx-1) {
                     array[i].add(idx)
                     array[i].add(idx-1)
                 }
                 else -> {
-                    if (a[idx-1] != i && !array[i].contains(idx-1)) {
+                    if (a[idx-1] != i && lastIndex != idx-1) {
                         array[i].add(idx-1)
                         array[i].add(idx)
-                    } else if (a[idx+1] != i && !array[i].contains(idx+1)) {
+                    } else if (a[idx+1] != i && lastIndex != idx+1) {
                         array[i].add(idx)
                         array[i].add(idx+1)
                     }
